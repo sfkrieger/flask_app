c69406319c4cad472365d36ad2c4ec1767c441df
sfkrieger <sfkrieger@gmail.com>
2014-10-13 18:29:05 -0400

    Sammys additions

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+#To render a template you can use the render_template() method.
+# Provide the name of the template and the variables you want to pass to the template engine as keyword arguments.
 @app.route('/')
 def hello_world():
-    return render_template('index.html')
+    return render_template('child_template.html')
 
 if __name__ == '__main__':
     app.run()
